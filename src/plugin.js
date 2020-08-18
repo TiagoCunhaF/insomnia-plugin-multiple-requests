cc521eba7368df4c764eb5a73b7ed4c1f4c798ec
TCUNHA <tiago.cunhabr.ibm.com@TCUNHAs-MacBook-Pro.local>
2020-08-18 01:59:36 -0300

    feat: add response time

--- src/plugin.js
+++ src/plugin.js
@@ -4,6 +4,18 @@ module.exports.requestGroupActions = [
     action: async (context, data) => {
       const { requests } = data;
 
+      function formatElapsedTime(elapsedTime) {
+
+        if (elapsedTime < 1000)
+          return `${Math.round(elapsedTime)} ms`;
+
+        if (elapsedTime < 1000 * 60)
+          return `${Math.round(elapsedTime / 1000 * 100) / 100} s`;
+
+        return `${Math.round(elapsedTime / (1000 * 60) * 100) / 100} m`;
+
+      }
+
       let results = [];
       for (const request of requests) {
         const response = await context.network.sendRequest(request);
@@ -12,13 +24,13 @@ module.exports.requestGroupActions = [
             ? "padding:5px; color: var(--color-font-danger); background: var(--color-danger)"
             : "padding:5px; color: var(--color-font-success); background: var(--color-success)";
         results.push(
-          `<li style="margin: 5px">${request.name}: <span style="${style}")>${response.statusCode}</span></li>`
+          `<li style="margin: 8px; height: 20px"><div style="width: 250px; display: inline-block;">${request.name}</div> <span style="${style}")>${response.statusCode} - ${formatElapsedTime(response.elapsedTime)}</span></li>`
         );
       }
 
       const html = `<ul style="column-count: ${
         results.length > 10 ? 2 : 1
-      };">${results.join("\n")}</ul>`;
+        };">${results.join("\n")}</ul>`;
 
       context.app.showGenericModalDialog("Results", { html });
     },
